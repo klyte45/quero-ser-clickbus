f9b5fe16887d65895331c7ab2ba8a2bb626d7ba4
Leandro Klyte <klyte45@hotmail.com>
2019-10-06 14:20:29 -0300

    Swagger configuration

--- testes/backend-developer/solution/src/main/java/com/klyte/places/controllers/PlacesController.java
+++ testes/backend-developer/solution/src/main/java/com/klyte/places/controllers/PlacesController.java
@@ -3,6 +3,9 @@ package com.klyte.places.controllers;
 import com.klyte.places.dto.PlaceDTO;
 import com.klyte.places.dto.PlaceRequestDTO;
 import com.klyte.places.services.PlaceService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,34 +15,51 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/places")
+@RequestMapping(value = "/places", produces = "application/json", consumes = "application/json")
 public class PlacesController {
 
     @Autowired
     private PlaceService placeService;
 
     @GetMapping
+    @ApiOperation("List all places stored")
     public ResponseEntity<List<PlaceDTO>> list() {
         return new ResponseEntity<>(placeService.listPlaces(), HttpStatus.OK);
     }
 
     @GetMapping("/{slug}")
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "Place not found"),
+    })
+    @ApiOperation("Get a specific place by its slug")
     public ResponseEntity<PlaceDTO> get(@PathVariable("slug") String urlSlug) {
         return new ResponseEntity<>(placeService.getPlace(urlSlug), HttpStatus.OK);
     }
 
     @PutMapping("/{slug}")
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "Place not found"),
+            @ApiResponse(code = 400, message = "Invalid data sent"),
+    })
+    @ApiOperation("Update a place data")
     public ResponseEntity<PlaceDTO> update(@PathVariable("slug") String urlSlug, @RequestBody @Valid PlaceRequestDTO place) {
         return new ResponseEntity<>(placeService.updatePlace(urlSlug, place), HttpStatus.OK);
     }
 
     @PostMapping
+    @ApiResponses({
+            @ApiResponse(code = 400, message = "Invalid data sent"),
+    })
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation("Create a new place")
     public ResponseEntity<PlaceDTO> create(@RequestBody @Valid PlaceRequestDTO place) {
         return new ResponseEntity<>(placeService.createPlace(place), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{slug}")
-    public ResponseEntity<PlaceDTO> delete(@PathVariable("slug") String urlSlug) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ApiOperation("Delete a place")
+    public ResponseEntity<Void> delete(@PathVariable("slug") String urlSlug) {
         placeService.deletePlace(urlSlug);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
--- testes/backend-developer/solution/src/main/java/com/klyte/places/dto/ErrorDTO.java
+++ testes/backend-developer/solution/src/main/java/com/klyte/places/dto/ErrorDTO.java
@@ -1,5 +1,6 @@
 package com.klyte.places.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,7 +9,10 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class ErrorDTO {
+    @ApiModelProperty(value = "Exception name", example = "Exception")
     private String errorTitle;
+    @ApiModelProperty(value = "Exception detail", example = "Exception has occurred")
     private String description;
+    @ApiModelProperty(value = "Status code generated", example = "500")
     private Integer statusCode;
 }
